74246403036c63935b848fefbe503119a8b515e2
alexandrazuenco <alexandra.zuenco@gmail.com>
2018-11-13 19:04:46 +0200

    SU-011 Add array method task

---
+++ array.js
@@ -0,0 +1,9 @@
+var myArray =  ["HTML", 11, "JavaScript", 2, "CSS", 23, 12];
+myArray.push(-5);
+console.log ('result', myArray);
+myArray.unshift(1000);
+console.log ('result', myArray);
+myArray.push('Last array element');
+console.log ('result', myArray);
+myArray.splice(3, 1);
+console.log ('result', myArray);
